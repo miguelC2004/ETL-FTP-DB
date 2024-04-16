adb0c1339277dd3a49d4280affa49842c4566369
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2024-04-16 09:25:28 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ScriptAsesorDigital',
+    version='2.0',
+    packages=[''],
+    url='https://github.com/miguelC2004/ETL---FTP',
+    license='MIT',
+    author='miguelcatano.3',
+    author_email='miguel.catano@conceptbpo.com ',
+    description='Script de automatización para inserción de data de una FTP a base de datos, configurable para los demás informes'
+)
